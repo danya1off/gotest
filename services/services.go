6726cb42595d6302d39535ef826af9a32a1093ca
Jeyhun Danyaloff <jeyhun.danyaloff@gmail.com>
2019-12-05 22:43:56 +0200

    Code changes

--- services/services.go
+++ services/services.go
@@ -2,13 +2,13 @@ package services
 
 import (
 	"encoding/json"
-	"github.com/danya1off/library/utils"
+	"github.com/danya1off/library/models"
 	"net/http"
 )
 
-func GetAllBooks(env *utils.Env) http.HandlerFunc {
+func GetAllBooks(db *models.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		books, err := env.DB.GetAllBooks()
+		books, err := db.GetAllBooks()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
